2d67c4b61f080156dacfa2ffaaecd8c7e937d7b2
Shulamith-Aujero <98571193+Shulamith-Aujero@users.noreply.github.com>
2022-01-28 18:43:08 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,128 @@
+<html>
+<header> <title> TLE Scaffold 1; 3rd Quarter </title> </header>
+
+<body background="lol.gif"> 
+<center>
+<img src="header.png" height="280" width="1350">
+
+<br>
+<br>
+<br>
+<br>
+
+<img src="teachers.png" height="280" width"850">
+<table border="3" cellpadding="13" cellspacing="5">
+<tr>
+	<td bgcolor="9CECFF"> <font size="18"> <a href="file:///C:/Users/Lenovo/Desktop/3RD%20QUARTER%20PERFORMANCE%20TASK%20IN%20MATHEMATICS%209.html">Click here to access Performance Task </a>
+</tr> </table> </font>
+<br>
+<br>
+<br>
+<br>
+
+
+<img src="miss lili.png" height="280" width"850">
+<table border="3" cellpadding="13" cellspacing="5">
+<tr> 
+	<td bgcolor="CCB5FB"> <font size="18"> <a href=" ">Click here to access Performance Task</a>
+</tr> </table> </font>
+
+<br>
+<br>
+<br>
+<br>
+
+<img src="sir ariel.png" height="280" width"850">
+<table border="3" cellpadding="13" cellspacing="5">
+<tr>
+	 
+	<td bgcolor="#c0eca3"><font size="30"> <a href=" ">Click here to access Performance Task </a>
+ <tr> </table> </font>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+
+
+<font face="Cooper">
+<font size="7">
+
+<font color="#416B3E">
+G
+</font>
+
+<font color="#4D814A">
+r
+</font>
+
+<font color="#5D9B59">
+o
+</font>
+
+<font color="#73AD70">
+u
+</font>
+
+<font color="#83B67F">
+p
+</font>
+
+<font color="#97C294">
+ 1
+</font>
+
+<font color="#B0D1AE">
+5
+</font>
+
+
+<br>
+<br>
+<br>
+<marquee scroll amount="15" direction="right">
+<font size="6">
+
+<center>
+<font color="#FDFD96">
+<img src="me.jpg" heigth="80" width="80"> <br>
+Shulamith M. Aujero
+</center>
+</font>
+<br> <br>
+
+<center>
+<font color="#D5AAFF">
+<img src="abi.jpg" heigth="80" width="80"> <br>
+Abigail M. Potestad
+</center>
+</font>
+<br> <br>
+
+<center>
+<font color="#98E690">
+<img src="katrina.jpg" heigth="80" width="80"> <br>
+Katrina Gwen Cobias
+</font>
+</center>
+<br> <br>
+
+<center>
+<font color="#FFB449">
+<img src="misakichi.jpg" heigth="80" width="80"> <br>
+Misakichi A. Alunan
+</center>
+</font>
+<br> <br>
+
+<center>
+<font color="#B5B9FF">
+<img src="janine.jpg" heigth="80" width="80"> <br>
+Janine Ysabelle Olvido
+</center>
+
+</marquee> </font>
+</body>
+</html>
